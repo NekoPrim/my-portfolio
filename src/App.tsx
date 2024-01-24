7c868f5eb5814d496f8afa9e5b1092202aba106b
Desiree Quade <nekodez@gmail.com>
2024-01-24 17:00:09 -0600

    added menu, added more photos, removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,70 @@
-import React from 'react';
-// import { ScreenSize } from './app/ScreenSize';
+import { useState } from 'react';
+import { ScreenSize } from './app/ScreenSize';
+import { Alert, Dropdown, NavItem, NavLink } from 'react-bootstrap';
+
 import { About } from './features/about/About';
 import { Projects } from './features/projects/Projects';
 import { Resume } from './features/resume/Resume';
 import { Contact } from './features/contact/Contact';
 import { Credits } from './features/credits/Credits';
 import './App.css';
-import { Container, NavDropdown, Navbar } from 'react-bootstrap';
 
 function App() {
-  // const { windowDimension } = ScreenSize();
+  const { windowDimension } = ScreenSize();
+  const [show, setShow] = useState(true);
+  const [isOpen, setIsOpen] =useState<boolean>(false);
 
   return (
     <div className='App'>
-      <header className='App-header'>
+      <header className='App-header' id='1'>
         <div className='App-header-name'>
           <div>Desiree Quade</div>
           <div className='App-header-sub-name'>software engineer</div>
         </div>
       </header>
-      <Navbar>
-        <Container>
-          <NavDropdown
-            title='menu'
-            id='menu-dropdown'
-            className='menu'
-          >
-            <NavDropdown.Item
-              href='about'
-              className='menu-item'
-            >
-              about
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Container>
-      </Navbar>
+      { (windowDimension.width <= 550) ?
+        (<Alert
+            show={show}
+            variant='danger'
+            className='App-alert'
+            dismissible
+            onClick={() => setShow(false)}
+        >
+          <p>
+            Please rotate device to side for better viewing experience.
+          </p>
+        </Alert>)
+      : null}
+      <Dropdown as={NavItem} className='App-nav' role='button' autoClose='outside'>
+          <Dropdown.Toggle as={NavLink} data-bs-toggle='App-nav' className='App-nav-toggle'>
+            menu
+          </Dropdown.Toggle>
+          <Dropdown.Menu variant='dark' className='App-nav-menu' show={isOpen}>
+            <Dropdown.Item href='#1' className='App-nav-item' onSelect={() => setIsOpen(false)}>
+              top
+            </Dropdown.Item>
+            <Dropdown.Item href='#2' className='App-nav-item'>
+              projects
+            </Dropdown.Item>
+            <Dropdown.Item href='#3' className='App-nav-item'>
+              resume
+            </Dropdown.Item>
+            <Dropdown.Item href='#4' className='App-nav-item'>
+              contact
+            </Dropdown.Item>
+          </Dropdown.Menu>
+      </Dropdown>
 
       <About />
-      <Projects />
-      <Resume />
-      <Contact />
+      <div id='2'>
+        <Projects />
+      </div>
+      <div id='3'>
+        <Resume />
+      </div>
+      <div id='4'>
+        <Contact />
+      </div>
       <Credits />
 
     </div>
--- src/features/about/About.tsx
+++ src/features/about/About.tsx
@@ -25,7 +25,6 @@ export const About = () => {
                     </Nav.Link>
                 </div>
                 <div className='App-about-info'>
-                    {/* <img src={pupsAndI} className='App-image' alt='' /> */}
                     <div className='App-text'>
                         Driven by a passion for learning and a love for tackling new challenges, I bring a unique blend of skills and experiences to software engineering. My career path has taken me from assisting those with disabilities, to teaching English in Japan, to deciphering airplane blueprints, and even resolving customer telecommunications issues. I've managed quality assurance for incoming products, spearheaded operations for a research and development department, and have now embarked on the exciting journey of software development. My diverse background provides me with valuable perspectives and allows for seamless adaptation within any environment.
                     </div>
@@ -37,7 +36,6 @@ export const About = () => {
                 </div>
                 </div>
                 <div>
-                {/* <img src={pupsAndI} className='App-image' alt='' /> */}
                 { windowDimension.width <= 992 ?
                     <div className='App-game'>
                     {/* <MyButton text='GAME' action={onGame}/> */}
